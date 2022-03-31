4526f03cc16dc5fe42ac7b55226c135344357bae
JarlShadown <miguel.payran.del.angel@outlook.com>
2022-03-30 22:06:16 -0700

    add figure formulas

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("hello")
+const ladoCuadrado = 5;
+console.log(`Lados del cuadrado ${ladoCuadrado}`)
+
+const perimetroCuadrado = ladoCuadrado*4;
+console.log(`Perimetro del cuadrado es: ${perimetroCuadrado}cm`)
+
+const areaCuadrado = ladoCuadrado*ladoCuadrado;
+
+console.log(`Area del cuadrado es: ${areaCuadrado}cm^2`)
